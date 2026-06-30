8aaa3a96f17c2cb6c42534c63eff4ba0a36625ba
ANT-devolper <antonioguerreiroborges@gmail.com>
2026-06-29 23:57:30 -0300

    build: add ktlint for linting and formatting
    Apply the ktlint-gradle plugin (ktlint 1.8.0) to the app module and add an .editorconfig. Compose @Composable functions are excluded from the function-naming rule, since they use PascalCase by convention.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ktlint)
+}
+
+ktlint {
+    version.set(libs.versions.ktlint)
+    android.set(true)
 }
 
 android {
